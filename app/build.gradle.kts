a0ce79d74f1700ee51f0f9dfcb6191542372005c
AMillionDriver <nanangnurmansah5@gmail.com>
2026-05-26 12:33:15 +0700

    🔧 build: bump versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.axoloth.captaggenerator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 14
+        versionName = "1.8.6-ALPHA"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
